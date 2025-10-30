a8327b85d35e063c31d6b2b1e40ca61e66a8b3c2
Lahmar22 <lahmarzakariae22@gmail.com>
2025-10-30 11:52:44 +0100

    modify the card and fonction added and design

--- app.js
+++ app.js
@@ -1,43 +1,64 @@
-const money = document.getElementById('money');
-const toggleEye = document.getElementById('toggleEye');
-const eyeIcon = document.getElementById('eyeIcon');
+const money1 = document.getElementById('money1');
+const toggleEye1 = document.getElementById('toggleEye1');
+const eyeIcon1 = document.getElementById('eyeIcon1');
 
 const money2 = document.getElementById('money2');
 const toggleEye2 = document.getElementById('toggleEye2');
 const eyeIcon2 = document.getElementById('eyeIcon2');
 
-let actualMoney =0;
-const actualMoney2 = '73673123.45 MAD';
-let visible = false;
+const money3 = document.getElementById('money3');
+const toggleEye3 = document.getElementById('toggleEye3');
+const eyeIcon3 = document.getElementById('eyeIcon3');
+
+let TotalRevenus = 0 ;
+let SoldeNet = 0;
+let TotalDepenses =0 ;
+
+let visible1 = false;
+let visible2 = false;
+let visible3 = false;
 
 
 
-toggleEye.addEventListener('click', () => {
-    visible = !visible;
-    money.textContent = visible ? actualMoney : '****';
+toggleEye1.addEventListener('click', () => {
+    visible1 = !visible1;
+    money1.textContent = visible1 ? TotalRevenus : '****';
 
 
-    money.style.color = visible ? 'green' : 'black';
+    money1.style.color = visible1 ? 'green' : 'black';
 
 
-    eyeIcon.classList.toggle('bi-eye');
-    eyeIcon.classList.toggle('bi-eye-slash');
+    eyeIcon1.classList.toggle('bi-eye');
+    eyeIcon1.classList.toggle('bi-eye-slash');
 });
 
 toggleEye2.addEventListener('click', () => {
-    visible = !visible;
-    money2.textContent = visible ? actualMoney2 : '****';
+    visible2 = !visible2;
+    money2.textContent = visible2 ? SoldeNet : '****';
 
 
-    money2.style.color = visible ? 'green' : 'black';
+    money2.style.color = visible2 ? 'green' : 'black';
 
 
     eyeIcon2.classList.toggle('bi-eye');
     eyeIcon2.classList.toggle('bi-eye-slash');
 });
 
+toggleEye3.addEventListener('click', () => {
+    visible3 = !visible3;
+    money3.textContent = visible3 ? TotalDepenses : '****';
+
+
+    money3.style.color = visible3 ? 'green' : 'black';
+
+
+    eyeIcon3.classList.toggle('bi-eye');
+    eyeIcon3.classList.toggle('bi-eye-slash');
+});
+
 const nomVersement = document.getElementById('nomVersement');
 const montant = document.getElementById('montant');
+const typeSelect = document.getElementById('typemontant');
 const addBtn = document.getElementById('addBtn');
 const liste = document.getElementById('listeVersements');
 
@@ -45,24 +66,39 @@ addBtn.addEventListener('click', () => {
     const nom = nomVersement.value.trim();
     const prix = parseFloat(montant.value.trim()); 
     const date = new Date();
+    const type = typeSelect.value;
+
     const dateStr = date.toLocaleDateString('fr-FR', {
         day: '2-digit',
         month: '2-digit',
         year: 'numeric'
     });
 
-    if (nom === '' || prix === '') {
-        alert("Veuillez remplir les deux champs !");
+    if (nom === "" || prix === "" || type === "Type d'operation") {
+        alert("Veuillez remplir tous les champs !");
         return;
     }
-    actualMoney += prix;
     
+    
+    const typeText = type === "1" ? "Revenus" : "Dépenses";
+    const color = type === "1" ? "text-success" : "text-danger";
+    
+    if(typeText=='Revenus'){
+        TotalRevenus +=prix;
+        SoldeNet += prix;
+    }
+    else if(typeText=='Dépenses'){
+        SoldeNet -= prix;
+        TotalDepenses +=prix;
+
+    }
     const li = document.createElement('li');
     li.className = 'list-group-item d-flex justify-content-between align-items-center';
     li.innerHTML = `
         <div class="card1 d-flex gap-5">
-            <span><strong>${nom}</strong> | <strong class="text-success">+${prix} DH</strong></span>
+            <span><strong>${nom}</strong> | <strong class="${color}">+${prix} DH</strong></span>
             <p >${dateStr}</p>
+            <p >${typeText}</p>
             
         </div>
         <div class="btn-group">
